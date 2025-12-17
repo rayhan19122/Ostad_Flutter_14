6f512482425fc1d208b0fb8b6669002af7e6cb36
rayhan19122 <rayhan19122@gmail.com>
2025-12-17 23:47:50 +0600

    Reorganize files into Dart folder, reorganize project structure, add .DS_Store files

--- Dart/Dart_OOP/namedConstructorExample.dart
+++ Dart/Dart_OOP/namedConstructorExample.dart
@@ -1,9 +1,9 @@
 class User {
-  String name;
-  String email;
-  int age;
+  String? name;
+  String? email;
+  int? age;
 
-  User(this.name, this.email, this.age);
+  User({this.name, this.email, this.age});
   // User(String name, String email, int age) {
   //   this.name = name;
   //   this.email = email;
@@ -24,7 +24,7 @@ class User {
 }
 
 void main() {
-  var normalUser = User('Rayhan', 'rayhan19122@gmail.com', 23);
+  var normalUser = User(name: 'Rayhan', email: 'rayhan19122@gmail.com',age:23);
   var guestUser = User.guest();
   var dbUser = User.formJson({
     'name': 'Abir',
